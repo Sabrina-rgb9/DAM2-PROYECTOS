ccd6ec74e5701c55631630fa7e8d1e8c04f75e94
Sabrina-rgb9 <selmeftah3.cf@iesesteveterradas.cat>
2025-09-15 19:25:50 +0200

    calculadora

---
+++ Exemple 0000/src/main/java/com/project/Controller.java
@@ -0,0 +1,87 @@
+package com.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+
+
+    @FXML
+    private Button unoButton;
+    private Button dosButton;
+    private Button tresButton;
+    private Button cuatroButton;
+    private Button cincoButton;
+    private Button seisButton;
+    private Button sieteButton;
+    private Button ochoButton;
+    private Button nueveButton;
+    private Button ceroButton;
+    private Button sumButton;
+    private Button restButton;
+    private Button multiButton;
+    private Button diviButton;
+    private Button equalButton;
+    private Button clearButton;
+    private Button comaButton;
+
+
+
+    @FXML
+    private void actionUno(ActionEvent event) {
+        // Lógica para el botón "1"
+
+        
+        
+    }
+
+    @FXML
+    private void actionDos(ActionEvent event) {
+        // Lógica para el botón "2"
+
+    }
+
+    @FXML
+    private void actionTres(ActionEvent event) {
+        // Lógica para el botón "3"
+    }
+
+    @FXML
+    private void actionCuatro(ActionEvent event) {
+        // Lógica para el botón "4"
+    }
+
+    @FXML
+    private void actionCinco(ActionEvent event) {
+        // Lógica para el botón "5"
+    }
+
+    @FXML
+    private void actionSeis(ActionEvent event) {
+        // Lógica para el botón "6"
+    }
+
+    @FXML
+    private void actionSiete(ActionEvent event) {
+        // Lógica para el botón "7"
+    }
+
+    @FXML
+    private void actionOcho(ActionEvent event) {
+        // Lógica para el botón "8"
+    }
+
+    @FXML
+    private void actionNueve(ActionEvent event) {
+        // Lógica para el botón "9"
+    }
+
+    @FXML
+    private void actionCero(ActionEvent event) {
+        // Lógica para el botón "0"
+    }
+
+}
